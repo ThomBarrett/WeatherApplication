f37703aa9c82fe32f3be7cb2f8f03fc92e53110e
Thomas James Barrett <tom_72@outlook.com>
2022-06-09 00:52:03 +0100

    add test to make sure the temperatures make sense in relation to one and other

--- spec/models/temperature_setting_spec.rb
+++ spec/models/temperature_setting_spec.rb
@@ -14,4 +14,28 @@ RSpec.describe TemperatureSetting, type: :model do
   it 'validates presence of hot temperature' do
     expect(subject).to validate_presence_of(:hot_temperature)
   end
+
+  it 'validates cold temperature is less than warm temperature and hot temperature' do
+    subject.cold_temperature = 100
+    subject.warm_temperature = 25
+    subject.hot_temperature = 50
+
+    expect(subject.valid?).to eq false
+  end
+
+  it 'validates hot temperature is greater than cold temperature and warm temperature' do
+    subject.cold_temperature = 25
+    subject.warm_temperature = 50
+    subject.hot_temperature = 30
+
+    expect(subject.valid?).to eq false
+  end
+
+  it 'validates warm temperature is greater than cold temperature but colder than hot temperature' do
+    subject.cold_temperature = 0
+    subject.warm_temperature = 50
+    subject.hot_temperature = 100
+
+    expect(subject.valid?).to eq true
+  end
 end
