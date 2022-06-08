d2a93ff88feef92567cd4f739f96b3da25d83c7f
Thomas James Barrett <tom_72@outlook.com>
2022-06-09 00:52:55 +0100

    add validations to temperature settings so that temperatures will always make sense in relation to one and other

--- app/models/temperature_setting.rb
+++ app/models/temperature_setting.rb
@@ -2,6 +2,14 @@
 
 class TemperatureSetting < ActiveRecord::Base
   validates :cold_temperature, presence: true
+  validates :cold_temperature, numericality: { less_than: :warm_temperature }
+  validates :cold_temperature, numericality: { less_than: :hot_temperature }
+
   validates :warm_temperature, presence: true
+  validates :warm_temperature, numericality: { less_than: :hot_temperature }
+  validates :warm_temperature, numericality: { greater_than: :cold_temperature }
+
   validates :hot_temperature, presence: true
+  validates :hot_temperature, numericality: { greater_than: :cold_temperature }
+  validates :hot_temperature, numericality: { greater_than: :warm_temperature }
 end
