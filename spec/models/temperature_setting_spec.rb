129cb6b684d1b2f295d15ce607aff8b2e44d23b3
Thomas James Barrett <tom_72@outlook.com>
2022-06-09 00:14:59 +0100

    add test to check if TemperatureSetting Model validates presence true for all fields

--- spec/models/temperature_setting_spec.rb
+++ spec/models/temperature_setting_spec.rb
@@ -3,5 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe TemperatureSetting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validates presence of cold temperature' do
+    expect(subject).to validate_presence_of(:cold_temperature)
+  end
+
+  it 'validates presence of warm temperature' do
+    expect(subject).to validate_presence_of(:warm_temperature)
+  end
+
+  it 'validates presence of hot temperature' do
+    expect(subject).to validate_presence_of(:hot_temperature)
+  end
 end
