21357b2c0402012e8499e83464c1b67968a6cc7e
Thomas James Barrett <tom_72@outlook.com>
2022-06-09 00:16:34 +0100

    add presence validations to temperature_setting model for all fields

--- app/models/temperature_setting.rb
+++ app/models/temperature_setting.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class TemperatureSetting < ActiveRecord::Base
+  validates :cold_temperature, presence: true
+  validates :warm_temperature, presence: true
+  validates :hot_temperature, presence: true
 end
