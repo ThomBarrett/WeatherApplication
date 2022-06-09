1262ef2d9fa6b735af293fb9a7592b32b0756b15
Thomas James Barrett <tom_72@outlook.com>
2022-06-09 01:04:23 +0100

    fix rubocop offences

--- spec/models/temperature_setting_spec.rb
+++ spec/models/temperature_setting_spec.rb
@@ -3,23 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe TemperatureSetting, type: :model do
-  it 'validates presence of cold temperature' do
-    expect(subject).to validate_presence_of(:cold_temperature)
-  end
-
-  it 'validates presence of warm temperature' do
-    expect(subject).to validate_presence_of(:warm_temperature)
-  end
-
-  it 'validates presence of hot temperature' do
-    expect(subject).to validate_presence_of(:hot_temperature)
-  end
+  it 'validates presence of cold temperature' do expect(subject).to validate_presence_of(:cold_temperature) end
+  it 'validates presence of warm temperature' do expect(subject).to validate_presence_of(:warm_temperature) end
+  it 'validates presence of hot temperature' do expect(subject).to validate_presence_of(:hot_temperature) end
 
   it 'validates cold temperature is less than warm temperature and hot temperature' do
     subject.cold_temperature = 100
     subject.warm_temperature = 25
     subject.hot_temperature = 50
-
     expect(subject.valid?).to eq false
   end
 
@@ -27,7 +18,6 @@ RSpec.describe TemperatureSetting, type: :model do
     subject.cold_temperature = 25
     subject.warm_temperature = 50
     subject.hot_temperature = 30
-
     expect(subject.valid?).to eq false
   end
 
@@ -35,7 +25,6 @@ RSpec.describe TemperatureSetting, type: :model do
     subject.cold_temperature = 0
     subject.warm_temperature = 50
     subject.hot_temperature = 100
-
     expect(subject.valid?).to eq true
   end
 end
