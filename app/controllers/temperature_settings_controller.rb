387218b166545bf122f2c31ecea3a4dad6f526d6
Thomas James Barrett <tom_72@outlook.com>
2022-06-10 02:27:15 +0100

    create temperature settings controller

---
+++ app/controllers/temperature_settings_controller.rb
@@ -0,0 +1,25 @@
+class TemperatureSettingsController < ActionController::Base
+  def show
+    @temperature_setting = TemperatureSetting.first
+  end
+
+  def new
+    @temperature_setting = TemperatureSetting.new
+  end
+
+  def create
+    @temperature_setting = TemperatureSetting.new(temperature_setting_params)
+
+    if @temperature_setting.save
+      redirect_to @temperature_setting
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def temperature_setting_params
+    params.require(:temperature_setting).permit(:cold_temperature, :warm_temperature, :hot_temperature)
+  end
+end
