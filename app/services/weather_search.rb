58c6282837787c6bff171a2c6b9abf7f4998be1b
Thomas James Barrett <tom_72@outlook.com>
2022-06-11 20:45:22 +0100

    refactor weathersearch

--- app/services/weather_search.rb
+++ app/services/weather_search.rb
@@ -5,7 +5,8 @@ class WeatherSearch
   def self.search_by_postcode(postcode)
     key = 'b618835832eb470490c200325221006' # Change this to your own key
     request = "https://api.weatherapi.com/v1/current.json?key=#{key}&q=#{postcode}&aqi=no"
-    HTTParty.get(request).parsed_response
+    response = HTTParty.get(request)
+    JSON.parse(response.body)
   end
 
   def self.extract_temperature_from_json(json)
