425c53b54c2778e6af45563f99b4c56fd13d916f
Thomas James Barrett <tom_72@outlook.com>
2022-06-11 21:59:20 +0100

    tighten up temperaturecalculator unit testing

--- spec/services/temperature_calculator_spec.rb
+++ spec/services/temperature_calculator_spec.rb
@@ -3,18 +3,42 @@ require 'rails_helper'
 describe 'TemperatureCalculator' do
   describe '.how_hot_is_it' do
     subject { TemperatureCalculator }
-    let!(:temperature_setting) { create(:temperature_setting) }
+    context 'There are temperature settings' do
+      let!(:temperature_setting) { create(:temperature_setting) }
 
-    it 'receives a cold temperature and returns cold' do
-      expect(subject.how_hot_is_it(-5)).to eq 'cold'
+      it 'receives a cold temperature and returns cold' do
+        expect(subject.how_hot_is_it(-5)).to eq 'cold'
+      end
+
+      it 'receives a cold temperature and returns warm' do
+        expect(subject.how_hot_is_it(55)).to eq 'warm'
+      end
+
+      it 'receives a cold temperature and returns hot' do
+        expect(subject.how_hot_is_it(105)).to eq 'hot'
+      end
+
+      it 'receives nil temperature and returns unknown' do
+        expect(subject.how_hot_is_it(nil)).to eq 'unknown'
+      end
     end
 
-    it 'receives a cold temperature and returns warm' do
-      expect(subject.how_hot_is_it(55)).to eq 'warm'
-    end
+    context 'There are no temperature settings' do
+      it 'receives a cold temperature and returns unknown' do
+        expect(subject.how_hot_is_it(-5)).to eq 'unknown'
+      end
 
-    it 'receives a cold temperature and returns hot' do
-      expect(subject.how_hot_is_it(105)).to eq 'hot'
+      it 'receives a cold temperature and returns unknown' do
+        expect(subject.how_hot_is_it(55)).to eq 'unknown'
+      end
+
+      it 'receives a cold temperature and returns hot' do
+        expect(subject.how_hot_is_it(105)).to eq 'unknown'
+      end
+
+      it 'receives nil temperature and returns unknown' do
+        expect(subject.how_hot_is_it(nil)).to eq 'unknown'
+      end
     end
   end
 end
